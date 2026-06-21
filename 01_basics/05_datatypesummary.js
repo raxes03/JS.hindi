e926ee5c1469cf4cabc1fbdc2346fdbf8ed22231
RAKESH SINGH <rakeshsingh63845@gmail.com>
2026-06-21 08:33:57 +0000

    05_datatypesummary.js

---
+++ 01_basics/05_datatypesummary.js
@@ -0,0 +1,42 @@
+// primitive 
+/*7 types ke : 1string
+             2.Number
+             3.boolean
+             4.null
+             5.undefined
+             6.symbol
+             7.bigint*/
+
+             /*  non primitive
+             jisko hum refrences bhi bolte hai
+             Array
+             Objects
+             Functions
+
+
+             java script dynamic hai yha static 
+              In this, you do not need to specify the type of a variable (such as a number, string, or array) in advance. The type is determined automatically when the program runs, and you can store different types of data in the same variable.
+              */
+             const Score = 100
+            
+             const scoreValue = 100.3
+             const isLoggedIn = false
+             const outsideTemp = null
+             let userEmail;
+             const id = Symbol('1230')
+             const anotherId = Symbol('1230')
+             console.log(id==anotherId)
+
+               const bigNumber =1234567899876543456
+                const heros = ["shaktiman", "naagaraj","doga"];
+                let myobj = {
+                    name:"rakesh",
+                    age:22
+                }
+                    const myFunction=function(){
+                        console.log("Hello world");
+
+                    }
+                    console.log(typeof bifNumber);
+                     
+                
